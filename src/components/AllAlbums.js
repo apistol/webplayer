01ed07c30667f11bd7ad0466b4c8fd062b55ce38
EXT_API97 <EXT_API97@open-groupe.com>
2022-11-23 01:22:26 +0200

    Sidebar and SearchBar

---
+++ src/components/AllAlbums.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { albums } from '../assets/utils'
+
+const AllAlbums = () => {
+  return (
+    <div>
+        {albums.map(album => (<div>
+            
+        </div>))}
+    </div>
+  )
+}
+
+export default AllAlbums
